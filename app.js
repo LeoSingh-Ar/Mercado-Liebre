765a9d7b6c11f2431188a78feb541d5c08fa894e
LeoSingh-Ar <140126209+LeoSingh-Ar@users.noreply.github.com>
2023-08-15 13:12:01 -0300

    app.js

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const publicPath = path.resolve(__dirname,"./public")
 
 app.use(express.static(publicPath))
 
-app.listen(3000, () => {
+app.listen(3001, function() {
     console.log("server funcionando");
 })
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 
 app.get('/', function(req,res){
     res.sendFile(path.resolve(__dirname,'./views/index.html'))
